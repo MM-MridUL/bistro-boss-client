91423344df3f0f4d405ab52f4c4acfc83a0a97d5
MM-MridUL <mmmridul84@gmail.com>
2025-03-23 02:36:05 -0700

    Fixed login issue and API race condition

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -50,14 +50,16 @@ const auth = getAuth(app);
                 .then(res=>{
                     if(res.data.token){
                         localStorage.setItem('access-token',res.data.token);
+                        setLoading(false);
                     }
                 })
             }
             else{
                 //  TODO: remove token (if token stored in the client side:local storage, caching, in memory)
                 localStorage.removeItem('access-token');
+                setLoading(false);
             }
-            setLoading(false);
+           
         });
         return ()=>{
             return unsubscribe();
